525a5aaccbe0f32636821fe6953fef3419d44f37
Kris Kuchinka <kriskuchinka@gmail.com>
2023-08-15 15:32:28 -0700

    changes from before

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -1,33 +1,47 @@
 function CurrentWeather(props) {
   return (
-    <div>
-      <h1>
-        {props.city.name},{props.city.state}, {props.city.country}
-      </h1>
-      <h1>Current Temperature</h1>
-      <h3>{props.currentTemp}</h3>
+    <>
       <div>
+        <h1>
+          {props.city.name},{props.city.state}, {props.city.country}
+        </h1>
+        <h2>Expect: "{props.description}"</h2>
+        <h3>Current Temperature</h3>
+        <h4>{props.currentTemp}</h4>
         <div>
-          <h3>Insert Temp High</h3>
+          <div>
+            <h3>High Temperature:</h3>
+            <h4>{props.high}</h4>
+          </div>
+          <div>
+            <h3>Low Temperature:</h3>
+            <h4>{props.low}</h4>
+          </div>
         </div>
         <div>
-          <h3>Insert Temp Low</h3>
+          <div>
+            <h3>Sunrise:</h3>
+            <h4>{props.sunrise}</h4>
+          </div>
+          <div>
+            <h3>Sunset:</h3>
+            <h4>{props.sunset}</h4>
+          </div>
+        </div>
+        <div>
+          <img src={props.icon} />
         </div>
       </div>
       <div>
-        <div>
-          <h3>Sunrise</h3>
-          <h4>Insert Sunrise Time</h4>
-        </div>
-        <div>
-          <h3>Sunset</h3>
-          <h4>Insert Sunset Time</h4>
-        </div>
+        <h2>12 Hour Forecast</h2>
       </div>
       <div>
-        <p>Weather Image</p>
+        <div>
+          <h3>Hourly 1</h3>
+          <h4>Insert Hourly 1 Info</h4>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
